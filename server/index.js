67f24fc1b5ca8616ae59780e1b151814b06694cf
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2021-03-02 03:25:50 +0200

    added connection to database added db models

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
+require('dotenv').config()
 const express = require('express');
+const sequelize = require('./db');
+const models =require('./models/models.js');
+const cors = require('cors');
 
-const PORT = 5000;
 
-const app = express()
+const PORT = process.env.PORT || 5000;
 
-app.listen()
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
